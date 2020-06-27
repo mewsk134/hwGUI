06327f0f59f105ec799233134ffd2af21dd755bc
mewsk134 <mewsk134@hotmail.com>
2020-06-28 00:51:57 +0700

    added hw

---
+++ hwGUI/hwGUI/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hwGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int a = int.Parse(income.Text);
+            int b = int.Parse(expenditure.Text);
+            int c = int.Parse(wantPrice.Text);
+            int d = c/(a-b);
+            total.Text = d.ToString();
+            
+            
+
+
+        }
+    }
+}
